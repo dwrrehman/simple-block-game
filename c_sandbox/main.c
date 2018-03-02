82d273c47cbfb884ad58b49ba2c7fbd5d1dbc1b0
Daniel Rehman <dwrrehman@gmail.com>
2018-03-02 09:31:31 -0800

    Initial Commit

---
+++ c_sandbox/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_sandbox
+//
+//  Created by Daniel Rehman on 1803025.
+//  Copyright © 2018 Daniel Rehman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
